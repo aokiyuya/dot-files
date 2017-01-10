cdd675738aa655769a7aed0ac341546e2d934ca9
aokiyuya <yuya.aoki@fujielab.org>
2017-01-11 01:32:47 +0900

    add installer for dein

--- first_pull.sh
+++ first_pull.sh
@@ -3,6 +3,6 @@ cd ~/dot-files/
 git fetch
 git merge origin/master
 
-cp ~/dot-files/.vimrc ~/.vimrc
-cp -rf ~/dot-files/.vim/ ~/.vim/
+cp ~/dot-files/vimrc ~/.vimrc
+cp -rf ~/dot-files/vim/ ~/.vim/
 cp ~/dot-files/.latexmkrc ~/.latexmkrc
---
+++ install_dein.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+trap 'echo err' ERR
+
+mkdir -p ~/.cache/dein/
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh
+
+
+exit 0
+
